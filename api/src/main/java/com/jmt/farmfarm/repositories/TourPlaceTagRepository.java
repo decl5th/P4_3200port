d9092f916452c0ae40485b02ac975879bff25f8e
decl5th <alsgur6624@gmail.com>
2024-08-08 16:07:21 +0900

    3조 공공데이터 api 가져오기 캐치업

---
+++ api/src/main/java/com/jmt/farmfarm/repositories/TourPlaceTagRepository.java
@@ -0,0 +1,9 @@
+package com.jmt.farmfarm.repositories;
+
+import com.jmt.farmfarm.entities.TourPlaceTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourPlaceTagRepository extends JpaRepository<TourPlaceTag, String> {
+
+
+}
